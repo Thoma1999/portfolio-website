d323407d4f0019fb099a30238c52fe2f76bbe193
Thomas Laryea <Laryea.Thomas@gmail.com>
2022-10-18 20:36:40 +0100

    Setup project and create header

---
+++ components/Header.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center "
+      >
+        <SocialIcon
+          style={{ height: 30, width: 30 }}
+          className="cursor-pointer mr-3"
+          url="https://www.linkedin.com/in/thomas-laryea-6804b8108"
+          bgColor="black"
+        />
+        <SocialIcon
+          style={{ height: 30, width: 30 }}
+          className="cursor-pointer mr-3"
+          url="https://github.com/Thoma1999"
+          bgColor="black"
+        />
+        <SocialIcon
+          style={{ height: 30, width: 30 }}
+          className="cursor-pointer"
+          url="https://twitter.com/TomLaryea"
+          bgColor="black"
+        />
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-black-300 cursor-pointer"
+      >
+        <SocialIcon
+          style={{ height: 40, width: 40 }}
+          network="email"
+          bgColor="transparent"
+          fgColor="black"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-black-400">
+          CONTACT
+        </p>
+      </motion.div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Thomas Laryea</title>
+        <meta
+          name="description"
+          content="Software engineer portfolio website"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+    </div>
+  );
+};
+
+export default Home;
